ea586a7f845401b2d6fa1d89f15542417de61d42
Idan Goldman <idan@123code.co>
2022-06-05 12:15:41 +0300

    - update general style - created a first popup page version

--- src/javascripts/popup.js
+++ src/javascripts/popup.js
@@ -1,4 +1,5 @@
 import { Application } from '@hotwired/stimulus';
+import Controller from '~/src/javascripts/controllers/sites_list_controller';
 
 window.Stimulus = Application.start();
-Stimulus.register("sites", SitesController);
+Stimulus.register("sites_list", Controller);
