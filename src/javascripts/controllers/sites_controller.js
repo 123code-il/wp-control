18f921bae3050da301e3418453b3f1dc6032afcc
Idan Goldman <idan@123code.co>
2022-06-25 22:32:30 +0300

    remove unused targets

--- src/javascripts/controllers/sites_controller.js
+++ src/javascripts/controllers/sites_controller.js
@@ -4,8 +4,6 @@ import { SitesIndexRender } from '~/src/javascripts/renders/sites_index_render'
 import SiteModel from '~/src/javascripts/models/site_model'
 
 export default class SitesController extends Controller {
-  static targets = ['list']
-
   connect () {
     this.index()
   }
